8e080130514b6ad78671526544caea9348ae9445
m1dnas <felipesandimguliermone@gmail.com>
2023-01-08 11:29:05 -0300

    task 3 v1

---
+++ module1_task3/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2023-01-04T13:25:24-03:00
+draft: true
+---
+
---
+++ module1_task3/setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install hugo
+sudo apt-get install make
+make build
+exit 255
+"recipe for target 'build' failed" 2>&1

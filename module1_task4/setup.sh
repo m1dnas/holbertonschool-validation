ce9c109ec0811b1ecada6121a6102673750a55be
m1dnas <105666151+m1dnas@users.noreply.github.com>
2023-01-12 13:22:31 -0300

    task 4 v1

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,3 @@
 sudo apt-get update
-sudo apt-get install hugo
-sudo apt-get install make
-make build
-exit 255
-"recipe for target 'build' failed" 2>&1
+sudo apt-get install -y hugo make
+hugo new site
